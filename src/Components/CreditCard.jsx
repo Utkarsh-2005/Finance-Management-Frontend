634120d51d03b74fb30ff4a128fe4baf061f2030
Utkarsh <utkarshjha.4009@gmail.com>
2024-10-26 01:11:21 +0530

    Make credit card ftt screen

--- src/Components/CreditCard.jsx
+++ src/Components/CreditCard.jsx
@@ -5,7 +5,7 @@ import "./CreditCard.css";
 export default function CreditCard() {
   return (
     <div className="credit-card">
-      <Spline scene="https://prod.spline.design/wHXygQegwZ023Nnb/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/wHXygQegwZ023Nnb/scene.splinecode" className="card"/>
     </div>
   );
 }
